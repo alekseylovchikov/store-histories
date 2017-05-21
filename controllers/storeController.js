8bf38f73950a9c4b1418f8ccc6e0e5bb4fdd75e7
Aleksey Lovchikov <alelov@brabdev.com>
2017-05-21 17:29:43 +0300

    layout - add moment year to footer

---
+++ controllers/storeController.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
+exports.homePage = (req, res) => {
+  res.render('index', {
+    title: 'Home'
+  });
+};
+
+exports.addStore = (req, res) => {
+  res.render('editStore', { title: 'Добавить историю' });
+};
+
+exports.createStore = async (req, res) => {
+  const store = await (new Store(req.body)).save();
+  req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
+  res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+  const stores = await Store.find();
+  res.render('stores', { title: 'Все истории', stores });
+};
